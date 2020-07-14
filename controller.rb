6c1ea951ee86fd5198a87675bf8cac24990bd1ae
Lubdhak <lubdhakdon@gmail.com>
2020-07-14 21:54:05 +0530

    require relative

--- controller.rb
+++ controller.rb
@@ -1,4 +1,4 @@
-require '/Users/lubdhakmahapatra/Codebase/design_pattern/my_service_layer/root_client'
+require_relative './my_service_layer/root_client'
 include MyServiceLayer
 
 response = ::RootClient.new("hi","123")
@@ -6,3 +6,9 @@ review_service_id = response.user_details[:review_service_id]
 rating_service_id = response.user_details[:rating_service_id]
 puts response.review(review_service_id).review_details
 puts response.rating(rating_service_id).rating_details
+
+
+# To do
+# - common methods
+# - delegate methods
+# -
